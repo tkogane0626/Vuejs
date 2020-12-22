17c3991d8f3037c297059ff0651e772c7d6e3bdf
tkogane0626 <59507486+tkogane0626@users.noreply.github.com>
2020-12-22 13:02:01 +0900

    axiosを使ってサーバーにhttp通信をする

--- vuejs-http/src/App.vue
+++ vuejs-http/src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム</label>
+    <input
+      type="text"
+      id="name"
+      v-model="name"
+    >
+    <br>
+    <label for="comment">コメント：</label>
+    <textarea
+      id="comment"
+      v-model="comment"
+    ></textarea>
+    <br>
+    <button @click="createComment">コメントをサーバーに送る</button>
+    <h2>掲示板</h2>
+    <div v-for="post in posts" :key="post.name">
+      <div>名前：{{ post.fields.name.stringValue }}</div>
+      <div>コメント：{{ post.fields.comment.stringValue }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "./axios-auth"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: "",
+      comment: "",
+      posts: []
+    }
+  },
+  created(){
+    axios.get("/comments").then(response => {
+      this.posts = response.data.documents
+    })
+  },
+  methods: {
+    createComment() {
+      axios.post("/comments", {
+        fields: {
+          name: {
+            stringValue: this.name
+          },
+          comment: {
+            stringValue: this.comment
+          }
+        }
+      })
+      this.name = ""
+      this.comment = ""
+    }
   }
 }
 </script>
--- vuejs-http/src/main.js
+++ vuejs-http/src/main.js
@@ -1,7 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-http-c1e2d/databases/(default)/documents"
+
+const interceptorsRequest = axios.interceptors.request.use(
+  config => {
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+const interceptorsResponse = axios.interceptors.response.use(
+  config => {
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+axios.interceptors.request.eject(interceptorsRequest)
+axios.interceptors.response.eject(interceptorsResponse)
 
 new Vue({
   render: h => h(App),
